1ef8fcc747ce70fc6c2f209c1dd78e9bdede8412
Felistus Kimanthi <kimanthi.felistus@gmail.com>
2025-03-17 20:57:15 +0300

    added arrays to elements

--- index.js
+++ index.js
@@ -1,1 +1,39 @@
 // Write your solution here!
+
+let cats = ["Milo", "Otis", "Garfield"];
+
+function destructivelyAppendCat(Ralph) {
+  cats.push("Ralph");
+  return cats;
+}
+
+function destructivelyPrependCat(Bob) {
+  cats.unshift("Bob");
+  return cats;
+}
+
+function destructivelyRemoveLastCat() {
+  cats.pop();
+  return cats;
+}
+
+function destructivelyRemoveFirstCat() {
+  cats.shift();
+  return cats;
+}
+
+function appendCat(name) {
+  return [...cats, "Broom"];
+}
+
+function prependCat(name) {
+  return ["Arnold", ...cats];
+}
+
+function removeLastCat() {
+  return cats.slice(0, -1);
+}
+
+function removeFirstCat() {
+  return cats.slice(1);
+}
